39ca74a2d2b518bdc416af0deb38b9c3488c199c
Ivan Zakutniy <ivan.zakutny@flant.com>
2022-08-17 10:57:59 +0300

    111

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
+func handlerFunc(w http.ReponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Hello there! </h1>")
+}
 
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
