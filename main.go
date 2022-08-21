fbedde2c00dfe71a2bfcd66fcd2aa28dba927436
Ivan Zakutniy <ivan.zakutny@flant.com>
2022-08-21 13:29:39 +0300

    1 and 2 ex

--- main.go
+++ main.go
@@ -7,23 +7,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Hello there!!! </h1>")
+}
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1> Hello there!!! </h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@xyz.com\">support@xyz.com</a>.")
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@xyz.com\">support@xyz.com</a>.")
+}
 
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1> 404 not found !!!!!!</h1>")
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h4>Every frequently asked question is usually so stupid and obvious that it already contains the answer.</h4>")
+
+}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Contetn-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1> 404 not found !!!!!!</h1>")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/contact", contactHandler)
+	r.HandleFunc("/faq", faqHandler)
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	http.ListenAndServe(":3000", r)
 }
