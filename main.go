672dcfe988b935d8c7fa698fb264d25a007cb1c4
Ivan Zakutniy <ivan.zakutny@flant.com>
2022-08-21 10:24:31 +0300

    use Gorilla mux

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Hello there!!! </h1>")
+	w.Header().Set("Content-Type", "text/html")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1> Hello there!!! </h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@xyz.com\">support@xyz.com</a>.")
+
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> 404 not found !!!!!!</h1>")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/contact", handlerFunc)
+	http.ListenAndServe(":3000", r)
 }
