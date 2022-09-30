20dfc2e2f4a50774343f444cebcb508b96788480
Ivan Zakutniy <ivan.zakutny@flant.com>
2022-09-30 20:04:28 +0300

    update models

---
+++ models/users.go
@@ -0,0 +1,81 @@
+package models
+
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	ErrNotFound  = errors.New("models: resource not found")
+	ErrInvalidId = errors.New("models: Provided invalid object ID")
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	return &user, err
+}
+
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	return &user, err
+}
+
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return err
+}
+
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidId
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Delete(&user).Error
+}
+
+func (us *UserService) CloseConnection() error {
+	return us.db.Close()
+}
+
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
---
+++ models/users_test.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func testingUserService() (*UserService, error) {
+	const (
+		host     = "localhost"
+		port     = "5432"
+		user     = "admin"
+		password = "qwerty"
+		dbname   = "photogallery_test"
+		sslmode  = "disable"
+	)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	us, err := NewUserService(psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	us.db.LogMode(false)
+
+	us.DestructiveReset()
+	return us, nil
+}
+
+func TestCreateUser(t *testing.T) {
+	us, err := testingUserService()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := User{
+		Name:  "FooBar",
+		Email: "Foo@Bar.xx",
+	}
+	err = us.Create(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if user.ID == 0 {
+		t.Errorf("Expected User ID > 0. Received %d", user.ID)
+	}
+
+	if time.Since(user.CreatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected CreatedAt to be recent. Received %s", user.CreatedAt)
+	}
+
+	if time.Since(user.UpdatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected UpdatedAt to be recent. Received %s", user.CreatedAt)
+	}
+
+}
